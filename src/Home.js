ac2097160df88aa32d763bd293694bf9a5442894
nalgirerushikesh <nalgirerushikesh@gmail.com>
2024-05-21 17:12:15 +0530

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,209 @@
+import React from 'react'
+import './style.css';
+import formimage from './Images/formimage.jpg';
+import growth from './Images/growth.png';
+import instagram from './Images/instagram.png';
+function Home() {
+  return (
+    <div>
+        <div>
+  <section>
+    <div className="navbar">
+      <p className="logo">CrystalCode</p>
+      <div className="navlinks">Home</div>
+      <div className="navlinks">Services</div>
+      <div className="navlinks">Industries</div>
+      <div className="navlinks">Case Study</div>
+      <div className="navlinks">About Us</div>
+      <div className="navlinks">Contact Us</div>
+    </div>
+  </section>
+  <section>
+    <div className="hero">
+      <p className="statement">customized software solutions to boost your business</p>
+      <button className="btn">let's discuss your requirements</button>
+    </div>
+  </section>
+  <section>
+    <div className="excellent">
+      <div className="heading">We are <span>&nbsp;Excellent&nbsp;</span> in</div>
+      <div className="exc">
+        <div className="exc-card">
+          <button className="exc-cards">Web Development</button>
+          <button className="exc-cards">Web Development</button>
+          <button className="exc-cards">Web Development</button>
+          <button className="exc-cards">Web Development</button>
+          <button className="exc-cards">Web Development</button>
+        </div>
+        <div className="exc-content">
+          <div className="head">Web Development</div>
+          <div className="cont">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex omnis sit laudantium nobis amet ipsam recusandae deleniti ea quae, libero, blanditiis perspiciatis quasi? Animi omnis quae nihil eum fugit corporis! Tempore alias voluptatum vel odit odio sunt perferendis ipsa cum debitis fuga illo nobis dignissimos sint suscipit et aliquam unde, nam error dolore consequatur. Iste hic corporis, est voluptatibus natus esse, unde voluptate laboriosam libero voluptatum consequuntur aliquam vitae perspiciatis nemo enim laudantium iure doloremque. Animi consectetur quo esse aperiam maxime facere, incidunt obcaecati veniam eum, iusto voluptate quod cumque eligendi quibusdam reprehenderit molestias? Autem architecto cumque quia dolores consequuntur molestias, itaque debitis nulla officia sunt est porro laudantium ad corporis. Ea, dolorem laborum. Similique explicabo incidunt ad perspiciatis delectus, distinctio earum sunt eum eveniet autem, aliquam nesciunt! Esse at consequuntur soluta quam, velit optio harum obcaecati repellendus temporibus, nesciunt nostrum ullam in animi accusantium dolorum ipsam expedita dolorem? In labore, aut corrupti iure nesciunt rem magnam quaerat maxime ipsa vero maiores quia repellendus, sit aliquam, delectus pariatur. Beatae aliquid illo quasi a iste laudantium veniam animi ipsum, rem cumque blanditiis et ipsam, culpa rerum dolor optio. Alias aliquid eaque sapiente eum dignissimos, rem, voluptatum dolorum quaerat nostrum aperiam ut.</div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div className="choice">
+      <div className="heading">What makes us a <span>&nbsp;Right Choice</span></div>
+      <div className="choice-container">
+        <img src={growth} alt="" className="growth" />
+        <div className="skill-box">
+          <div className="lines">
+            <p />
+            <p />
+            <p />
+            <p />
+            <p />
+            <p />
+          </div>
+          <div className="skills">
+            <p>Specialized Skills</p>
+            <p>Specialized Skills</p>
+            <p>Specialized Skills</p>
+            <p>Specialized Skills</p>
+            <p>Specialized Skills</p>
+            <p>Specialized Skills</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div className="stack">
+      <div className="heading">Our <span>&nbsp;Tech-Stack</span></div>
+      <div className="stack-container">
+        <div>
+          <div className="stack-head">Frontend</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+        </div>
+        <div>
+          <div className="stack-head">Backend</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+        </div>
+        <div>
+          <div className="stack-head">DevOps</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+        </div>
+        <div>
+          <div className="stack-head">Cloud</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+        </div>
+        <div>
+          <div className="stack-head">Database</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+          <div className="stack-cont">React</div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div className="testimonial">
+      <div className="heading">What Our <span>&nbsp;Clients&nbsp;</span>Say</div>
+      <div className="one">
+        <div className="two">
+          <p className="two-cont">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique fugit ex praesentium quae enim impedit tempore provident eos. Pariatur temporibus est sint assumenda, officiis hic voluptas omnis! Accusamus fugit dicta veniam, voluptas delectus voluptatibus ducimus officia perferendis esse repellendus quidem eos corrupti odio, earum molestias quae facilis iste! Voluptas, neque?</p>
+          <p className="two-name">Rushikesh Nalgire</p>
+          <p className="two-designation">Co-founder<br /> Software Solutions</p>
+        </div>
+        <div className="two">
+          <p className="two-cont">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique fugit ex praesentium quae enim impedit tempore provident eos. Pariatur temporibus est sint assumenda, officiis hic voluptas omnis! Accusamus fugit dicta veniam, voluptas delectus voluptatibus ducimus officia perferendis esse repellendus quidem eos corrupti odio, earum molestias quae facilis iste! Voluptas, neque?</p>
+          <p className="two-name">Rushikesh Nalgire</p>
+          <p className="two-designation">Co-founder<br /> Software Solutions</p>
+        </div>
+        <div className="two">
+          <p className="two-cont">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique fugit ex praesentium quae enim impedit tempore provident eos. Pariatur temporibus est sint assumenda, officiis hic voluptas omnis! Accusamus fugit dicta veniam, voluptas delectus voluptatibus ducimus officia perferendis esse repellendus quidem eos corrupti odio, earum molestias quae facilis iste! Voluptas, neque?</p>
+          <p className="two-name">Rushikesh Nalgire</p>
+          <p className="two-designation">Co-founder<br /> Software Solutions</p>
+        </div>
+      </div>
+    </div></section>
+  <section>
+    <div className="tell">
+      <div className="heading">Tell Us Your <span>&nbsp;Needs</span></div>
+      <p className="tell-head">Fill the details below to hear back from us!</p>
+      <div className="container">
+        <div className="form-container">
+          <form>
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone Number:</label>
+              <input type="tel" id="phone" name="phone" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email Address:</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div className="form-group">
+              <textarea id="message" name="message" placeholder="Enter something about what your expectations" defaultValue={""} />
+            </div>
+            <div className="form-group">
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+        <div className="image">
+          <img src={formimage} alt="" />
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div className="footer">
+      <div className="heading">CONTACT US</div>
+      <p className="footer-head">We are pretty good in managing our clients!</p>
+      <div className="footer-container">
+        <div className="elements">
+          <div className="name">Email :</div>
+          <div className="value">crystalcodesoftwaresolutions@gmail.com</div>
+        </div>
+        <div className="elements">
+          <div className="name">Phone Number :</div>
+          <div className="value">+91 99999 88888</div>
+        </div>
+        <div className="elements">
+          <div className="icon"><img src={instagram} alt="" /></div>
+        </div>
+        <div className="elements">
+          <div className="icon"><img src={instagram} alt="" /></div>
+        </div>
+        <div className="elements">
+          <div className="icon"><img src={instagram} alt="" /></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div className="copyright">
+      <p className="url">www.crystalcoresolutions.com</p>
+      <p className="rights">© 2024 &nbsp;&nbsp;&nbsp;&nbsp; All rights reserved.</p>
+    </div>
+  </section>
+</div>
+
+    </div>
+  )
+}
+
+export default Home;
